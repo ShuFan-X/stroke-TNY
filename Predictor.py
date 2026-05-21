46e9807039c971aa68d6b4d1c21a5abc0862458d
ShuFan-X <xsfkkk8255@163.com>
2026-05-22 07:16:25 +0800

    Update Predictor.py

--- Predictor.py
+++ Predictor.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from lime.lime_tabular import LimeTabularExplainer
 #pip install xgboost==2.0.3 --no-deps
 
-df2 =pd.read_csv('stroke-TNY-x_test.csv.csv')
+df2 =pd.read_csv('stroke-TNY-x_test.csv')
 x_test = df2[['通脑饮', 'LAA', 'SAO', '年龄', '高血压', '吸烟史', '入院NIHSS', '入院mRS', 'NLR', '淋巴细胞', '尿素', '抗聚史', '降压史', '调脂史']]
 
 model = joblib.load('XGB.pkl')

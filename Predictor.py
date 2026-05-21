cc64adee5569e3a4868010dca244cbf0a1f93e6e
ShuFan-X <xsfkkk8255@163.com>
2026-05-22 07:18:39 +0800

    Update Predictor.py

--- Predictor.py
+++ Predictor.py
@@ -10,7 +10,7 @@ from lime.lime_tabular import LimeTabularExplainer
 df2 =pd.read_csv('stroke-TNY-x_test.csv')
 x_test = df2[['通脑饮', 'LAA', 'SAO', '年龄', '高血压', '吸烟史', '入院NIHSS', '入院mRS', 'NLR', '淋巴细胞', '尿素', '抗聚', '降压', '调脂']]
 
-model = joblib.load('XGB.pkl')
+model = joblib.load('stroke-TNY.pkl')
 
 feature_names = ['通脑饮', 'LAA', 'SAO', '年龄', '高血压', '吸烟史', '入院NIHSS', '入院mRS', 'NLR', '淋巴细胞', '尿素', '抗聚', '降压', '调脂']
     
